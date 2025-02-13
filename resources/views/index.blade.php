dfae6c7f7aa0dada6eda824f3ce587afffc0c517
ahmadaldraie <ahmad023853382@gmail.com>
2025-02-13 23:07:59 +0100

    feat: add index page

---
+++ resources/views/index.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="nl">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>KiemKracht opdracht</title>
+
+        <!-- Styles / Scripts -->
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    </head>
+    <body class="min-vh-100 d-flex align-items-center">
+        <div class="container d-flex flex-column flex-md-row justify-content-around align-items-center ">
+
+            <div>
+                <h1 class="title">KIEM<br>KRACHT<br>OPDRACHT</h1>
+            </div>
+            <div>
+                <a class="btn btn-c-index d-block mx-auto mb-3" href="/kassaticket">Een kassaticket sturen</a>
+                <a class="btn btn-c-index d-block mx-auto" href="/kassatickets">Kassatickets tonen</a>
+            </div>
+
+        </div>
+    </body>
+</html>
